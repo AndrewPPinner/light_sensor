ffa051035ac7c5a3e87d7a1ab15c4e41e324c630
AndrewPPinner <andppinner@gmail.com>
2024-10-11 17:50:22 -0400

    Adding some changes

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,21 @@
 #include <Wire.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <Creds.h>
+#include <ArduinoJson.h>
 
 BH1750 lightMeter(0x23);
-
-const char* ssid = "REPLACE_WITH_YOUR_SSID";
-const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+const char* _serverName = "https://andrew-pinner.asuscomm.com/smart/api/lightSensor";
+const char* _authServer = "https://andrewp.online/RCON/api/login";
+String _token = "";
 
 void setup() {
-
   Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
 
   // Initialize the I2C bus (BH1750 library doesn't do this automatically)
   Wire.begin();
@@ -24,10 +30,88 @@ void setup() {
   }
 }
 
-void loop() {
-  if (lightMeter.measurementReady()) {
-    float lux = lightMeter.readLightLevel();
-    //Send data to database
-    delay(600000);
+void GetAuth() {
+  String token;
+
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    WiFiClientSecure client;
+    client.setInsecure();
+    HTTPClient https;
+    https.useHTTP10(true);
+
+    if (https.begin(client, _authServer))
+    {
+      String request;
+      JsonDocument doc;
+      doc["Username"] = Username;
+      doc["Password"] = Password;
+
+      serializeJson(doc, request);
+
+      https.addHeader("Content-Type", "application/json");
+      int httpCode = https.POST(request);
+      token = https.getString();
+      Serial.println(token);
+      https.end();
+    }
+    else
+    {
+      Serial.println("Could not connect to server");
+    }
+  }
+
+  _token = token;
+}
+
+void UpdateSensor() {
+  float lux = lightMeter.readLightLevel();
+  WiFiClientSecure client;
+  client.setInsecure();
+  HTTPClient https;
+  https.useHTTP10(true);
+
+  if (_token.isEmpty())
+  {
+    GetAuth();
+  }
+  
+  if (https.begin(client, _serverName))
+  {
+    JsonDocument doc;
+    doc["Location"] = "";
+    doc["LightValue"] = lux;
+
+    String request;
+    serializeJson(doc, request);
+    Serial.println(request);
+
+    https.addHeader("Content-Type", "application/json");
+    https.addHeader("Authorization", "Bearer " + _token);
+    int httpCode = https.POST(request);
+    https.end();
+
+    if (httpCode == 401)
+    {
+      _token = "";
+      UpdateSensor();
+    }
+    
+  }
+  else
+  {
+    Serial.println("Could not connect to server");
+  }
+}
+
+void loop()
+{
+  if (lightMeter.measurementReady())
+  {
+    if (WiFi.status() == WL_CONNECTED)
+    {
+      UpdateSensor();
+    }
+    delay(6000);
   }
 }
